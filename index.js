94491a643dcd83e77342b72414f989aba6ce6500
Deepika Chouhan <31657134+deepika-chouhan@users.noreply.github.com>
2020-01-15 13:42:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,227 @@
+const debounce = (func, delay) => {
+  let debounceTimer;
+  return function() {
+    const context = this;
+    const args = arguments;
+    clearTimeout(debounceTimer);
+    debounceTimer = setTimeout(() => func.apply(context, args), delay);
+  }
+}
+(function(){
+  let movieData = [
+    {
+        "name": "Harrison Ford",
+        "movie": "Star Wars",
+        "character": "Han Solo",
+        "movieRating": 8
+    },
+    {
+        "name": "Carrie Fisher",
+        "movie": "Star Wars",
+        "character": "Princess Leia",
+        "movieRating": 8,
+        "profile": {
+          "profession": "actress",
+          "birthDate": "21 October 1956",
+          "Nationality": " American"
+        }
+    },
+    {
+        "name": "Al Pacino",
+        "movie": "The Godfather",
+        "character": "Michael Corleone",
+        "movieRating": 9
+    },
+    {
+        "name": "Robert De Niro",
+        "movie": "Goodfellas",
+        "character": "Jimmy Conway",
+        "movieRating": 7.8,
+        "profile": {
+          "profession": "Actor",
+          "birthDate": "17 August 1943",
+          "Nationality": " American",
+          "spouseName": "Grace Hightower"
+        }
+    }
+  ]
+  const { thead, tbody } = displayTable(movieData);
+  let table = document.querySelector('#movieTable');
+  table.appendChild(thead);
+  table.appendChild(tbody);
+  // const box = paginator({
+  //   table,
+  //   getRows: document.querySelectorAll('tbody tr'),
+  //   box: document.getElementById("index_native"),
+  // });
+  document.getElementById("myInput").addEventListener('input', debounce(searchTable, 200))
+})();
+
+// Remove sort attributes
+function removeSortAttrs() {
+  let header = document.querySelector("th[data-sortorder]");
+  if (header) {
+    header.lastElementChild.removeAttribute('class');
+    header.removeAttribute('data-sortorder');
+  }
+};
+
+function sortHeader(e) {
+  if (e.target.type === 'text') { // filter input
+    return;
+  }
+  let targetHeader = e.target;
+  let isAsc = targetHeader.getAttribute('data-sortorder');
+  let tdId = parseInt(targetHeader.getAttribute('id'));
+  if (isAsc === 'asc') {
+    targetHeader.lastElementChild.setAttribute('class', 'down');
+    targetHeader.setAttribute('data-sortorder', 'desc')
+  } else {
+    removeSortAttrs();
+    targetHeader.lastElementChild.setAttribute('class', 'up');
+    targetHeader.setAttribute('data-sortorder', 'asc')
+  }
+
+  var table, rows, switching, i, x, y, shouldSwitch;
+  table = document.getElementById("movieTable");
+  switching = true;
+  /*Make a loop that will continue until
+  no switching has been done:*/
+  while (switching) {
+    //start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /*Loop through all table rows (except the
+    first, which contains table headers):*/
+    for (i = 1; i < (rows.length - 1); i++) {
+      //start by saying there should be no switching:
+      shouldSwitch = false;
+      /*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+      x = rows[i].getElementsByTagName("td")[tdId];
+      y = rows[i + 1].getElementsByTagName("td")[tdId];
+      //check if the two rows should switch place:
+      if (
+        (isAsc === null || isAsc === 'desc') && x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase() ||
+        isAsc === 'asc' && x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()
+        ) {
+        //if so, mark as a switch and break the loop:
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
+}
+
+
+function searchTable() {
+  handleFilter();
+  removeSortAttrs();
+};
+
+function handleFilter() {
+  let trs = document.querySelectorAll('tbody tr');
+  let inputElements = document.querySelectorAll("input:not(#myInput)");
+  let filteredInputElements = [...inputElements].filter(input => input.value.trim() !== '');
+  inputValue = document.getElementById("myInput").value.toLowerCase();
+
+    let filteredTrs = [...trs].filter(tr => {
+      let showSearchRow = inputValue !== '' ? tr.innerText.toLowerCase().includes(inputValue) : true;
+      let showfilteredRow = filteredInputElements.every(input => {
+        let filterId = parseInt(input.parentElement.id);
+          return tr.childNodes[filterId].innerText.toLowerCase().includes(input.value.toLowerCase().trim());
+      })
+      if (showSearchRow && showfilteredRow) {
+        tr.style.display = "";
+        return tr;
+      } else {
+        tr.style.display = "none";
+      }
+    })
+    // paginator({
+    //   getRows: filteredTrs,
+    //   box: document.getElementById("index_native"),
+    // })
+}
+function moreTable(data) {
+  var modal = document.getElementById("myModal");
+  let moreTable = document.getElementById('moreTable');
+  modal.style.display = "block";
+  var span = document.getElementsByClassName("close")[0];
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+    moreTable.innerHTML = ''
+  }
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+      moreTable.innerHTML = ''
+    }
+  }
+  const { thead, tbody } = displayTable([data], false);
+  moreTable.appendChild(thead);
+  moreTable.appendChild(tbody);
+}
+
+function displayTable(movieData, isMovieTable = true) {
+  let headers = Object.keys(...movieData);
+  let thead = document.createElement('thead');
+  let tbody = document.createElement('tbody');
+  let tr = document.createElement('tr');
+  for(i=0; i<headers.length; i++) { // create header
+    let th = document.createElement('th');
+    let textNode = document.createTextNode(headers[i].split(/(?=[A-Z])/).join(" "));
+    th.appendChild(textNode);
+   
+    if (isMovieTable) {
+      let input = document.createElement('input');
+      let icon = document.createElement('i'); // sort icon
+      th.setAttribute("id", i);
+      th.appendChild(input);
+      input.addEventListener("change", handleFilter);
+      th.appendChild(icon);
+    }
+    tr.appendChild(th);
+    if (isMovieTable) {
+      th.addEventListener('click', sortHeader)
+    }
+  }
+
+  thead.appendChild(tr);
+  movieData.forEach(data => {
+    let tr = document.createElement('tr');
+    headers.forEach(header => {
+      let td = document.createElement('td');
+      let textNode = document.createTextNode(data[header]);
+      td.appendChild(textNode);
+      tr.appendChild(td);
+    })
+    let objData = Object.values(data).find(d => typeof d === 'object');
+    if (objData) {
+      let td = document.createElement('td', {
+        dataObj: objData,
+      });
+      let textNode = document.createTextNode('More');
+      td.appendChild(textNode);
+      td.setAttribute("id", 'more')
+      td.addEventListener('click', function() {
+        moreTable(objData);
+        return false;
+      });
+      tr.appendChild(td);
+    }
+    tbody.appendChild(tr);
+  })
+  return {
+    thead,
+    tbody,
+  }
+}
---
+++ paginator.js
@@ -0,0 +1,182 @@
+function paginator(config) {
+    console.log("paginator")
+    // throw errors if insufficient parameters were given
+    if (typeof config != "object")
+        throw "Paginator was expecting a config object!";
+
+     // get/set if things are disabled
+    if (typeof config.disable == "undefined") {
+        config.disable = false;
+    }
+
+    // get/make an element for storing the page numbers in
+    var box;
+    if (!(config.box instanceof Element)) {
+        config.box = document.createElement("div");
+    }
+    box = config.box;
+
+    var trs = config.getRows;
+    if (typeof config.rows_per_page == "undefined") {
+        config.rows_per_page = 2;
+    } 
+    var rows_per_page = config.rows_per_page;
+    
+     // get/set current page
+    if (typeof config.page == "undefined") {
+        config.page = 1;
+    }
+    var page = config.page;
+
+    // get page count
+    var pages = (rows_per_page > 0)? Math.ceil(trs.length / rows_per_page):1;
+    // check that page and page count are sensible values
+    if (pages < 1) {
+        pages = 1;
+    }
+    if (page > pages) {
+        page = pages;
+    }
+    if (page < 1) {
+        page = 1;
+    }
+    config.page = page;
+
+     // hide rows not on current page and show the rows that are
+     for (var i=0;i<trs.length;i++) {
+        if (typeof trs[i]["data-display"] == "undefined") {
+            trs[i]["data-display"] = trs[i].style.display||"";
+        }
+        if (rows_per_page > 0) {
+            if (i < page*rows_per_page && i >= (page-1)*rows_per_page) {
+                trs[i].style.display = trs[i]["data-display"];
+            } else {
+                // Only hide if pagination is not disabled
+                if (!config.disable) {
+                    trs[i].style.display = "none";
+                } else {
+                    trs[i].style.display = trs[i]["data-display"];
+                }
+            }
+        } else {
+            trs[i].style.display = trs[i]["data-display"];
+        }
+    }
+
+    // page button maker functions
+    config.active_class = config.active_class||"active";
+    if (typeof config.box_mode != "function" && config.box_mode != "list" && config.box_mode != "buttons") {
+        config.box_mode = "button";
+    }
+    if (typeof config.box_mode == "function") {
+        config.box_mode(config);
+    } else {
+        var make_button;
+        if (config.box_mode == "list") {
+            make_button = function (symbol, index, config, disabled, active) {
+                var li = document.createElement("li");
+                var a  = document.createElement("a");
+                a.href = "#";
+                a.innerHTML = symbol;
+                a.addEventListener("click", function (event) {
+                    event.preventDefault();
+                    this.parentNode.click();
+                    return false;
+                }, false);
+                li.appendChild(a);
+
+                var classes = [];
+                if (disabled) {
+                    classes.push("disabled");
+                }
+                if (active) {
+                    classes.push(config.active_class);
+                }
+                li.className = classes.join(" ");
+                li.addEventListener("click", function () {
+                    if (this.className.split(" ").indexOf("disabled") == -1) {
+                        config.page = index;
+                        paginator(config);
+                    }
+                }, false);
+                return li;
+            }
+        } else {
+            make_button = function (symbol, index, config, disabled, active) {
+                var button = document.createElement("button");
+                button.innerHTML = symbol;
+                button.addEventListener("click", function (event) {
+                    event.preventDefault();
+                    if (this.disabled != true) {
+                        config.page = index;
+                        paginator(config);
+                    }
+                    return false;
+                }, false);
+                if (disabled) {
+                    button.disabled = true;
+                }
+                if (active) {
+                    button.className = config.active_class;
+                }
+                return button;
+            }
+        }
+
+        // make page button collection
+        var page_box = document.createElement(config.box_mode == "list"?"ul":"div");
+        if (config.box_mode == "list") {
+            page_box.className = "pagination";
+        }
+
+        var left = make_button("&laquo;", (page>1?page-1:1), config, (page == 1), false);
+        page_box.appendChild(left);
+
+        for (var i=1;i<=pages;i++) {
+            var li = make_button(i, i, config, false, (page == i));
+            page_box.appendChild(li);
+        }
+
+        var right = make_button("&raquo;", (pages>page?page+1:page), config, (page == pages), false);
+        page_box.appendChild(right);
+        if (box.childNodes.length) {
+            while (box.childNodes.length > 1) {
+                box.removeChild(box.childNodes[0]);
+            }
+            box.replaceChild(page_box, box.childNodes[0]);
+        } else {
+            box.appendChild(page_box);
+        }
+    }
+     // make rows per page selector
+     if (!(typeof config.page_options == "boolean" && !config.page_options)) {
+        if (typeof config.page_options == "undefined") {
+            config.page_options = [
+                { value: 5,  text: '5'   },
+                { value: 10, text: '10'  },
+                { value: 20, text: '20'  },
+                { value: 50, text: '50'  },
+                { value: 100,text: '100' },
+                { value: 0,  text: 'All' }
+            ];
+        }
+       
+    }
+    // status message
+    var stat = document.createElement("span");
+    stat.innerHTML = `Showing Page ${page} of ${pages}`
+    box.appendChild(stat);
+
+    // hide pagination if disabled
+    if (config.disable) {
+        if (typeof box["data-display"] == "undefined") {
+            box["data-display"] = box.style.display||"";
+        }
+        box.style.display = "none";
+    } else {
+        if (box.style.display == "none") {
+            box.style.display = box["data-display"]||"";
+        }
+    }
+    return box;
+}
